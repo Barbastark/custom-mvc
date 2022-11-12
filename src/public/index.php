15cecbc0ad14602429e0501d9abb8747be9b1b68
Barbastark <dorkinacage@gmail.com>
2022-11-12 22:07:38 +0100

    Basic directory structure

---
+++ src/public/index.php
@@ -0,0 +1,14 @@
+<?php
+
+$num = 533;
+$str = "Hello";
+
+echo "$str I'm $num years old haha!!";
+
+/*$pdo = new PDO('mysql:dbname=mvc;host=mysql', 'root', '1234', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
+$query = $pdo->query('SHOW VARIABLES like "version"');
+
+$row = $query->fetch();
+
+echo 'MySQL version:' . $row['Value'];*/
